10e37d295175b945ff07b7adf13a9e197a7c563e
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-12-09 23:49:19 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import DontMiss from "@/components/DONTMISS";
+import Essential from "@/components/ennisial";
+// import Featuredfrom "@/components/FEATURED";
+import GearUp from "@/components/gearup";
+import Hero from "@/components/HERO";
+import Navigation from "@/components/Navigation";
+import { CarouselSize } from "@/components/BEST"
+import Featured from "./sidebae.tsx/page";
+
+
+export default function Home() {
+  return (
+  <main>
+ 
+    <Hero/>
+    <CarouselSize/>
+    <Featured/>
+    <GearUp/>
+    <DontMiss/>
+    <Essential/>
+    <Navigation/>
+  </main>
+  )
+}
